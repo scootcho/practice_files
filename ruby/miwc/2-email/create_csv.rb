4518ef7ac9af393a1dff0696637d1198d310dfda
Scott Yu <scott.tj.yu@gmail.com>
2014-08-24 16:08:27 +0800

    deleted files manually

--- ruby/miwc/2-email/create_csv.rb
+++
@@ -1,8 +0,0 @@
-require 'csv'
-
-CSV.open("my_results.csv", "wb") do |csv|
-  csv << ["firstname", "lastname", "phone", "email"]
-  csv << ["scott", "yu", "888-8888", "scott@robots.com"]
-  # ...
-end
-
